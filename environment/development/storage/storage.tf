c1e0df6e00fb9918c075165b5ff75e773229b717
Javier <awoisoak@gmail.com>
2023-01-13 19:35:13 +0900

    Solve input passed to internal storage module

--- environment/development/storage/storage.tf
+++ environment/development/storage/storage.tf
@@ -1,8 +1,7 @@
 module "storage" {
   source                      = "../../../modules/storage"
-  project                     = var.project_id
-  force_destroy               = var.bucket_force_destroy
-  location                    = var.region
+  project_id                  = var.project_id
+  region                      = var.region
   uniform_bucket_level_access = var.uniform_bucket_level_access
   versioning                  = var.versioning
   bucket_name                 = var.bucket_name
